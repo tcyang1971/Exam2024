c31d0f5c9954212e86d9c8f72ebb07ed8c1f0d93
tcyang1971 <tcyang@gm.pu.edu.tw>
2024-05-12 14:47:05 +0800

    v4_楊子青

--- app/src/main/java/tw/edu/pu/csim/tcyang/exam2024/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/exam2024/MainActivity.kt
@@ -1,5 +1,7 @@
 package tw.edu.pu.csim.tcyang.exam2024
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,7 +10,9 @@ import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
@@ -29,6 +33,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -117,7 +122,87 @@ fun Introduce(navController: NavController) {
 
 @Composable
 fun Organization(navController: NavController) {
-    Text( text = "主要機構", color = Color.Red)
+    var flag by remember { mutableStateOf(1) }  // 愛心家園與瑪利亞學園切換
+    val context = LocalContext.current  //取得App的運行環境
+
+    Column(modifier = Modifier
+        .fillMaxSize(),
+    )
+    {
+        Text(text="主要機構", color = Color.Red)
+        Row {
+
+
+            Button(
+                onClick = { flag = 1},
+            ) {
+                Text(
+                    text = "台中市愛心家園",
+                )
+            }
+
+            Button(
+                onClick = { flag = 2},
+            ) {
+                Text(
+                    text = "瑪利亞學園",
+                )
+            }
+        }
+
+        if (flag ==1){
+            Text(
+                text = "「台中市愛心家園」經市政府公開評選後，委託瑪利亞基金會經營管理，於91年啟用，整棟建築物有四個樓層，目前開辦就醫、就養、就學、就業四大領域的十項業務，提供身心障礙者全方位的服務。"
+            )
+
+            Text(
+                text = "\n長按以下圖片，可以觀看愛心家園地圖",
+                color = Color.Blue
+            )
+
+            Image(
+                painterResource(id = R.drawable.lovehome),
+                contentDescription ="愛心家園圖示",
+                modifier = Modifier
+                    .pointerInput(Unit) {
+                        detectTapGestures(
+                            onLongPress = {
+                                var it = Intent(Intent.ACTION_VIEW)
+                                it.data = Uri.parse("geo:0,0?q=台中市南屯區東興路一段450號")
+                                context.startActivity(it)
+                            }
+                        )
+                    }
+            )
+        }
+
+        else{
+            Text(
+                text = "「瑪利亞學園」提供重度以及極重度多重障礙者日間照顧服務，以健康照護為基礎，支持生活多面向參與及學習概念，輔助發展重度身心障礙者自我概念為最終服務目標。"
+            )
+
+            Text(
+                text = "\n雙擊以下圖片，可以觀看瑪利亞學園地圖",
+                color = Color.Blue
+            )
+
+            Image(
+                painterResource(id = R.drawable.campus),
+                contentDescription ="瑪利亞學園圖示",
+                modifier = Modifier
+                    .pointerInput(Unit) {
+                        detectTapGestures(
+                            onDoubleTap = {
+                                var it = Intent(Intent.ACTION_VIEW)
+                                it.data = Uri.parse("geo:0,0?q=台中市北屯區經貿東路365號")
+                                context.startActivity(it)
+                            }
+                        )
+                    }
+
+            )
+        }
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
